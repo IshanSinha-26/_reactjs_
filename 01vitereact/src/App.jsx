180c3c6eb463d090a1609e898f90e872c72bb35c
IshanSinha-26 <ishansinha2611@gmail.com>
2026-05-09 15:32:27 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header"
+function App() {
+  return (
+    <div>
+      <h1>Hello my friend!!!!!!</h1>
+      <h1>My name is Ishan Sinha</h1>
+      <Header />
+    </div>
+  )
+}
+export default App
