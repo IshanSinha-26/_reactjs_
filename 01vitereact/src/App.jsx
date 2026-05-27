db456b6fef7bd65fb172bf080cb7003202a3af47
IshanSinha-26 <ishansinha2611@gmail.com>
2026-05-28 04:06:57 +0530

    `Removed unused import and code in App.jsx and deleted vite.config.js`

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,46 @@
-import Header from "./Header"
-function App() {
-  return (
+// import Header from "./Header"
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello my friend!!!!!!</h1>
+//       <h1>My name is Ishan Sinha</h1>
+//       <Header />
+//     </div>
+//   )
+// }
+// export default App
+
+//**************************State and hooks********************************
+// import { useState } from "react";
+// import Counter from "./counter";
+// function App(){
+//   const[fruit,setFruit]=useState("mango");
+//   const handleClick=()=>{
+//     setFruit("banana");
+//   }
+//   return(
+//     <div>
+//       <h1>which fruit you like the most?</h1>
+//       <p>{fruit}</p>
+//       <button onClick={handleClick}>Change Fruit</button>
+//       <Counter/>
+//     </div>
+//   )
+// }
+// export default App;
+
+//************************* toggle ***************************************
+import { useState } from "react";
+function App(){
+  const[display,setDisplay]=useState(true);
+  return(
     <div>
-      <h1>Hello my friend!!!!!!</h1>
-      <h1>My name is Ishan Sinha</h1>
-      <Header />
-    </div>
+      <button onClick={()=>setDisplay(!display)}>Toggle</button>
+      {
+        display ? <h1>Toggle Example</h1> : null
+      }
+      
+      </div>
   )
 }
-export default App
+export default App;
---
+++ 01vitereact/src/counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+function Counter(){
+    const[count,setCount]=useState(0);
+    const handleClick=()=>{
+        setCount(count+1);
+    }
+    return(
+        <div>
+            <p>{count}</p>
+            <button onClick={handleClick}>Increment</button>
+        </div>
+    )
+}
+export default Counter;
